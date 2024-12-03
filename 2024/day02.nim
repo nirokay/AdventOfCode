5cca7f1b6a1b08d40e5321a736dbf4c03aa4bf54
nirokay <nirokay-public@protonmail.com>
2024-12-03 10:28:53 +0100

    2024 day 02 part 2

--- 2024/day02.nim
+++ 2024/day02.nim
@@ -1,4 +1,4 @@
-import std/[strutils, algorithm]
+import std/[strutils, algorithm, sequtils]
 import utils
 
 var reports: seq[seq[int]]
@@ -12,14 +12,8 @@ for line in getInputLines(2):
     for rawLevel in rawLevels:
         reports[^1].add rawLevel.parseInt()
 
-
-# -----------------------------------------------------------------------------
-# Part 1:
-# -----------------------------------------------------------------------------
-
-var unsafeReports: seq[int]
-for reportId, levels in reports:
-    var safe: bool = true
+proc isReportSafe(levels: seq[int]): bool =
+    result = true
     var differences: seq[int]
     for i, level in levels[1 .. ^1]:
         let prev: int = levels[i]
@@ -27,17 +21,44 @@ for reportId, levels in reports:
 
     # Drastic in-/decrement:
     for diff in differences:
-        if diff notin 1 .. 3: safe = false
+        if diff notin 1 .. 3: result = false
     # Wobbling measurements:
-    if levels notin [levels.sorted(Ascending), levels.sorted(Descending)]: safe = false
-
-    if not safe: unsafeReports.add reportId
-
-solution($(reports.len() - unsafeReports.len()) & " / " &  $reports.len(), "Safe reports from the reactor")
+    if levels notin [levels.sorted(Ascending), levels.sorted(Descending)]: result = false
 
 
 # -----------------------------------------------------------------------------
 # Part 1:
 # -----------------------------------------------------------------------------
 
+var unsafeReports: seq[int]
+for reportId, levels in reports:
+    if not levels.isReportSafe(): unsafeReports.add reportId
+
+let firstPartSolution: int = reports.len() - unsafeReports.len()
+solution($firstPartSolution & " / " &  $reports.len(), "Safe reports from the reactor")
+
+
+# -----------------------------------------------------------------------------
+# Part 2:
+# -----------------------------------------------------------------------------
+
+var nevermindActuallySafeTrustMeBro: seq[int]
+
+proc recheckSafety(reportId: int): bool =
+    var actualSafety: seq[bool]
+    let levels: seq[int] = reports[reportId]
+
+    for i in 0 .. levels.len() - 1:
+        var newLevels: seq[int] = levels
+        newLevels.delete(i)
+        actualSafety.add newLevels.isReportSafe()
+
+    result = true in actualSafety
+
+for id in unsafeReports:
+    let safe: bool = id.recheckSafety()
+    if safe: nevermindActuallySafeTrustMeBro.add id
+
+let partTwoSolution: int = firstPartSolution + nevermindActuallySafeTrustMeBro.len()
+solution($partTwoSolution & " / " &  $reports.len(), "Nvm, actually safe reports from the reactor")
 
