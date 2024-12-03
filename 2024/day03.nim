554dce38c154b199c829c0d4aaf77944669ab434
nirokay <nirokay-public@protonmail.com>
2024-12-03 11:16:20 +0100

    2024 day 03 part 2 - i am caught up :3

--- 2024/day03.nim
+++ 2024/day03.nim
@@ -3,26 +3,44 @@ import utils
 
 let
     memory: string = getInput(3).strip()
-    pattern: Regex = re"mul\(\d{1,3},\d{1,3}\)"
+    part1pattern: Regex = re"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)"
 
 var expressions: seq[string]
-for expression in memory.findAll(pattern):
+for expression in memory.findAll(part1pattern):
     expressions.add expression
 
-var multiplications: seq[array[2, int]]
-for expression in expressions:
-    let
-        commaSeparated: seq[string] = expression.split(",")
-        left: string = commaSeparated[0].split("(")[^1]
-        right: string = commaSeparated[^1].split(")")[0]
-    multiplications.add [left.parseInt(), right.parseInt()]
+proc parseInstructions*(enableConditions: bool): seq[array[2, int]] =
+    var skipInstructions: bool = false
+    for expression in expressions:
+        case expression:
+        of "do()": skipInstructions = false
+        of "don't()": skipInstructions = true
+        else:
+            if enableConditions and skipInstructions: continue
+            let
+                commaSeparated: seq[string] = expression.split(",")
+                left: string = commaSeparated[0].split("(")[^1]
+                right: string = commaSeparated[^1].split(")")[0]
+            result.add [left.parseInt(), right.parseInt()]
+
 
 # -----------------------------------------------------------------------------
 # Part 1:
 # -----------------------------------------------------------------------------
 
-var results: seq[int]
-for expression in multiplications:
-    results.add expression[0] * expression[1]
+var solutionPartOne: seq[int]
+for expression in parseInstructions(enableConditions = false):
+    solutionPartOne.add expression[0] * expression[1]
 
-solution(results.sum(), "Sum of all multiplications")
+solution(solutionPartOne.sum(), "Sum of all multiplications")
+
+# -----------------------------------------------------------------------------
+# Part 2:
+# -----------------------------------------------------------------------------
+
+var solutionPartTwo: seq[int]
+for expression in parseInstructions(enableConditions = true):
+    solutionPartTwo.add expression[0] * expression[1]
+
+solution(solutionPartTwo.sum(), "Sum of all multiplications with conditions")
+
