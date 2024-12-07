7430b38367d3272c23f853072e0cac516a2853bc
nirokay <nirokay-public@protonmail.com>
2024-12-07 19:23:37 +0100

    i tried optimizing my code, but it runs just aas slow as before ;-;

--- 2024/day07.nim
+++ 2024/day07.nim
@@ -1,4 +1,4 @@
-import std/[strutils, math, algorithm, sequtils]
+import std/[strutils, math, algorithm, sequtils, tables]
 import utils
 
 let
@@ -15,14 +15,20 @@ type
         numbers*: seq[int]
         operations*, validOperations*: seq[seq[Operation]]
 
+var cacheTable: Table[string, seq[int]]
 proc toBase(number, base: int, minLength: int = 0): seq[int] =
     ## Converts an `int` into a list of digits of base `n`
-    result = @[]
-    var buffer: int = number
-    while buffer > 0:
-        let remainder: int = buffer mod base
-        buffer = buffer div base
-        result.add remainder
+    let cacheId: string = $number & "@" & $base
+    if cacheTable.hasKey(cacheId):
+        result = cacheTable[cacheId]
+    else:
+        result = @[]
+        var buffer: int = number
+        while buffer > 0:
+            let remainder: int = buffer mod base
+            buffer = buffer div base
+            result.add remainder
+        cacheTable[cacheId] = result
     while result.len() < minLength:
         result.add 0
     result.reverse()
@@ -68,13 +74,24 @@ proc validateOperations(expression: var Expression) =
             operations: seq[Operation] = originalOperations.reversed()
             numbers: seq[int] = expression.numbers.reversed()
             solution: int = numbers.pop()
-
         while operations.len() > 0 and numbers.len() > 0:
             let number: int = numbers.pop()
             case operations.pop():
             of Multiplication: solution *= number
             of Addition: solution += number
-            of Concatenation: solution = parseInt($solution & $number)
+            of Concatenation:
+                # Unsure which operation is quicker:
+                solution = parseInt($solution & $number)
+                #[
+                solution *= (
+                    if number div 10_000 > 0: 100_000
+                    elif number div 1_000 > 0: 10_000
+                    elif number div 100 > 0: 1_000
+                    elif number div 10 > 0: 100
+                    else: 10
+                )
+                solution += number
+                ]#
         if solution == expression.targetValue:
             expression.validOperations.add originalOperations
             expression.validOperations = expression.validOperations.deduplicate()
