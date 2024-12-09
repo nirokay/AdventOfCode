dfc15cc116de7345cd777561c704b32a50a3c49c
nirokay <nirokay-public@protonmail.com>
2024-12-09 14:05:40 +0100

    2024 day 09 part 1

---
+++ 2024/day09.nim
@@ -0,0 +1,64 @@
+import std/[options, strutils, strformat]
+import utils
+
+let inputRaw: string = getInputStripped(9)
+
+type
+    Block = object
+        id*: int
+    Disk = seq[Option[Block]]
+
+proc `$`*(component: Option[Block]): string =
+    result = block:
+        if component.isNone(): "."
+        else: "[" & $component.get().id & "]"
+proc `$`*(disk: Disk): string =
+    result = disk.join("")
+
+let originalDisk: Disk = block:
+    var result: Disk
+    for i, c in inputRaw:
+        let
+            isFile: bool = i mod 2 == 0
+            fileId: int = i div 2
+            length: int = parseInt($c)
+            insertedData: Option[Block] = block:
+                if isFile: some Block(id: fileId)
+                else: none Block
+        for _ in 1 .. length:
+            result.add insertedData
+    result
+
+
+# -----------------------------------------------------------------------------
+# Part 1:
+# -----------------------------------------------------------------------------
+
+proc moveFileBlocksLeft(disk: var Disk) =
+    var farthestDataBlockIndex: int = disk.len() - 1
+    iterator locateEmptyIndex(disk: Disk): int =
+        for i in 0 .. disk.len() - 1:
+            if i >= farthestDataBlockIndex: break
+            if disk[i].isSome(): continue
+            yield i
+    proc moveFarthestDataBlock(disk: var Disk, target: int) =
+        for curr in countdown(farthestDataBlockIndex, 0):
+            if disk[curr].isNone(): continue
+
+            disk[target] = disk[curr]
+            disk[curr] = none Block
+
+            farthestDataBlockIndex = curr
+            return
+    for emptyId in disk.locateEmptyIndex():
+        disk.moveFarthestDataBlock(emptyId)
+
+var veryCompactedDisk: Disk = originalDisk
+veryCompactedDisk.moveFileBlocksLeft()
+
+var partOneSolution: int
+for position, fileBlock in veryCompactedDisk:
+    if fileBlock.isNone(): break
+    partOneSolution += position * fileBlock.get().id
+
+solution(partOneSolution, "Checksum of packed disk")
